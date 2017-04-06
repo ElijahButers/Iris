68a1ea34b34a50ccefd5acdb996c4b7138d3174c
ElijahButers <Elijahbuters@gmail.com>
2017-04-06 20:49:13 +0300

    Added code to the viewDidLoad() to the ViewController where we first set a translation transform between each replication, then take the negative value of dotOffset for the translation on the X axis because we want the copies to progress from right to left.

--- Iris/ViewController.swift
+++ Iris/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
     replicator.addSublayer(dot)
     
     replicator.instanceCount = Int(view.frame.size.width / dotOffset)
+    replicator.instanceTransform = CATransform3DMakeTranslation(-dotOffset, 0.0, 0.0)
   }
   
   @IBAction func actionStartMonitoring(_ sender: AnyObject) {
